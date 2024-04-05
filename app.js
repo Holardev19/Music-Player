ab75bd14939d6044ad2a499fba9e39c8aabe561f
Oladimeji Adeyemo <holarthedevtitan@gmail.com>
2024-04-05 23:14:06 +0100

    Updated music player

---
+++ app.js
@@ -0,0 +1,8 @@
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+const sidebar = document.querySelector(".container .sidebar");
+
+menuOpen.addEventListener("click", () => sidebar.style.left = "0");
+
+menuClose.addEventListener("click", () => sidebar.style.left = "-100");
+
